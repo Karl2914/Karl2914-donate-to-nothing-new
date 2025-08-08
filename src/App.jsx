68c2a6341645da35d3bf1dc9cb137c9333a3ee82
Karl2914 <alyoshass@ukr.net>
2025-08-08 12:19:47 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Welcome to Nothing.</h1>
+      <p>
+        It's simple: send us crypto and receive nothing in return.<br />
+        What is this website about? Well, that's a secret. Or not.<br />
+        We're just here for the fun, or maybe for the absurdity.<br />
+        Enjoy wasting your time. We do it all the time.
+      </p>
+      <img src="/meme.jpg" style={{ width: '100%', maxWidth: 500 }} alt="meme" />
+    </div>
+  );
+}
